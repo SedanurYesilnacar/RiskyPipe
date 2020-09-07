68b80ffeaebe92286b2469755b93056bf6f426af
salihcan kahya <salihcankahya@gmail.com>
2020-09-07 21:45:06 +0300

    Player Movement && Player Scaling

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    private void Start()
+    {
+        rb.velocity = new Vector3(0, 0, 10);
+    }
+}
